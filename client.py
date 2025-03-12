f2f1a1a3c4b1e99b4c8c22e90f7846fbb3edc6c4
CassioAA <cassioalmeidaccti@gmail.com>
2025-03-12 09:33:52 -0300

    feat: sending text file

--- client.py
+++ client.py
@@ -7,10 +7,16 @@ port = 8080
 address = (host, port)
 sock.connect(address)
 
-bufsize = 1024
-response_bytes = sock.recv(bufsize)
-while response_bytes:
-    print(response_bytes.decode('utf-8'))
-    response_bytes = sock.recv(bufsize)
+file_name = 'aa'
+file_name_bytes = bytes(file_name, 'utf-8')
+sock.send(file_name_bytes)
+
+buffer_size = 1024
+file_content_bytes = sock.recv(buffer_size)
+file_content = file_content_bytes.decode('utf-8')
+with open(file_name, "w", encoding="utf-8") as f:
+    f.write(file_content)
+print("Content of the received file:")
+print(file_content)
 
 sock.close()
--- server.py
+++ server.py
@@ -9,13 +9,20 @@ sock.bind(address)
 
 backlog = 1
 sock.listen(backlog)
-
-print('the server is listening for a connection')
+print('the server is listening to clients')
 
 conn, addr = sock.accept()
+print("connection established with", str(addr))
 
-response_message = "hi, we are connected o/"
-response_bytes = bytes(response_message, 'utf-8')
-conn.send(response_bytes)
+buffer_size = 1024
+file_name = conn.recv(buffer_size)
+try:
+    file_bytes_reader = open(file_name, 'rb')
+    file_content_bytes = file_bytes_reader.read()
+    conn.send(file_content_bytes)
+    file_bytes_reader.close()
+except:
+    querying_error_bytes = bytes('File not found here on the server', 'utf-8')
+    conn.send(querying_error_bytes)
 
 conn.close()
